c78806865268f6952c3ae7e7167daeddb4601bff
Moominimim <jgan0624@uni.sydney.edu.au>
2023-10-27 20:42:41 +1100

    master

--- w10.js
+++ w10.js
@@ -1,10 +1,11 @@
 //Defining Variables
 let song;
 let img;
-let fft; //Fast Fourier Transform objects for audio visualisation
+let fft; //Fast Fourier Transform object for audio visualisation
 let button; //Play and pause buttons
+let sizeSlider, colorSlider;   
 
-// Add in music and images before you start
+// Preloading music and image assets
 function preload() {
     song = loadSound('sample-visualisation.mp3');
     img = loadImage('16 Reasons To Love (And Respect) The Ocean.jpeg');
@@ -14,20 +15,30 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
+    colorMode(HSL); //Setting HSL color mode
     song.play();
     fft = new p5.FFT(0.8, 256); //Initializing the FFT object for audio analysis
 
     //Creating and positioning the PLAY/PAUSE button
     button = createButton('PLAY / PAUSE');
     button.id('playButton');
-    let buttonY = (height/2) + 300;
+    let buttonY = (height/2) + 270;
     button.position(width/2 - button.width/2, buttonY);
 
+    // Create sliders for user control size and color 
+    let sliderYOffset = 100; // Set the vertical offset for the first slider
+    sizeSlider = createSlider(1, 50, 8, 1); //Set the size slider range from 1 to 50 and default value of 8
+    sizeSlider.position(width/2 - sizeSlider.width/2, buttonY + sliderYOffset); //Place it below the button
+    
+    sliderYOffset += 70;
+    colorSlider = createSlider(50, 100, 75, 1); //Set the color slider range from 50%-100% and defult value of  75%
+    colorSlider.position(width/2-colorSlider.width/2, buttonY + sliderYOffset); // Place it below the size slider
+
     //The togglePlay function is called when the button is clicked.
-    button.mousePressed(togglePlay);
+    button.mousePressed(togglePlay);    
 }
 
-//Toggle the play and pause state of the song
+//Toggle between playing and pausing the song
 function togglePlay() {
     if (song.isPlaying()) {
         song.stop();
@@ -37,13 +48,23 @@ function togglePlay() {
 }
 
 function draw() {
-    image(img, 0, 0, width, height); //Drawing background images
+    image(img, 0, 0, width, height); // Display the background image to cover the canvas
     
+    // Display the lables for size and color control
+    textSize(23);
+    fill(255);
+    text("Change Size", sizeSlider.x + sizeSlider.width + 12, sizeSlider.y + 16);
+    text("Change Color", colorSlider.x + colorSlider.width + 12, colorSlider.y + 16);
+
     //Analyzing the song's audio frequencies
     let spectrum = fft.analyze();
     noStroke();
     translate(width / 2, height / 2); //Move the drawing origin to the centre of the screen
     
+    //Capture user input for visualization controls
+    let size = sizeSlider.value();
+    let hueValue = colorSlider.value();
+
     //Looping through the spectrum to generate the visual patterns
     for (let i = 0; i < spectrum.length; i ++) {
         let angle = map(i, 0, spectrum.length, 0, 360);//Mapping Spectrum Data to Angles
@@ -57,8 +78,9 @@ function draw() {
         for (let j = 0; j < 360; j += 37) {  
             push();
             rotate(j);
-            fill(255, 255, 255, amp);  // Setting the kaleidoscope colour with HSL color
-            ellipse(x, y, 8);
+            let luminanceValue = colorSlider.value();
+            fill(180, 100, luminanceValue, amp);  // Hue is set to 180, Saturation is set to 100% and Luminance is determined by the slider
+            ellipse(x, y, size);
             pop(); //Restore the previous state of the canvas
         }   
     }
